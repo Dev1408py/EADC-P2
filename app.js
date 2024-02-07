3c55b713732e1d6580ec33eb812bb7ccf8803d5d
dev <devpatel14082003@gmail.com>
2024-02-07 15:40:53 +0530

    Changes updated

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by dev to test pipeline and this is the second version');
-  });
+  res.sendFile(__dirname+'/index.html');
+});
   const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
